3708e3ad4ddd315d40724d64e5152a0b4bee8efe
William B <54190172+BlacknallW@users.noreply.github.com>
2019-09-07 23:31:37 -0400

    Delete pattern_exercises.py
    Accidental upload

--- pattern_exercises.py
+++
@@ -1,9 +0,0 @@
-#return whether two arrays have the same frequency of characters
-array1 = [8,0,4,4]
-array2 = [8,4,0,4]
-
-def frequency_calculator(array1, array2):
-    if sorted(array1) == sorted(array2):
-        return True
-    else:
-        return False
